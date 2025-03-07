4e577540214baf14bd2dcf199cba3318f68e9582
Ulises <larrabeulises@gmail.com>
2025-03-07 10:14:39 -0300

    add Orders module with DTOs, entity, controller, and service

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,12 @@
+import { IsDateString, IsNumber, IsString } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsString()
+  local: string;
+  @IsNumber()
+  totalPrice: number;
+  @IsString()
+  paymentMethod: 'cash' | 'mercado_pago' | 'pedidos_ya';
+  @IsDateString()
+  createdAt: Date;
+}
---
+++ src/orders/entities/order.entity.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type OrdersDocument = HydratedDocument<Order>;
+
+@Schema()
+export class Order {
+  @Prop({ required: true })
+  local: string;
+  @Prop({ required: true })
+  totalPrice: number;
+  @Prop({ required: true })
+  createdAt: Date;
+  @Prop({ required: true })
+  paymentMethod: 'cash' | 'mercado_pago' | 'pedidos_ya';
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
