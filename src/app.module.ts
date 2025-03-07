736003e1d2943705fb5e31b1170b542e42f54c74
Ulises <larrabeulises@gmail.com>
2025-03-07 10:14:01 -0300

    add modules for flavours, movements, and orders to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FlavoursModule } from './flavours/flavours.module';
+import { MovementsModule } from './movements/movements.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -14,6 +17,9 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: configService.get('MONGO_URL'),
       }),
     }),
+    FlavoursModule,
+    MovementsModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
